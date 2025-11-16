e4d58f392d9426d8a02204038b3b9e1aba5b5a5b
Zeverouis <Zeverouis@gmail.com>
2025-11-16 22:15:30 +0100

    Added image dto's

---
+++ src/main/java/nl/ploentuin/ploentuin/dto/image/ImageCreateDto.java
@@ -0,0 +1,21 @@
+package nl.ploentuin.ploentuin.dto.image;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ImageCreateDto {
+
+    @NotEmpty(message = "Mag niet leeg zijn")
+    private MultipartFile[] images;
+
+    private String[] captions;
+
+}
---
+++ src/main/java/nl/ploentuin/ploentuin/dto/image/ImageResponseDto.java
@@ -0,0 +1,25 @@
+package nl.ploentuin.ploentuin.dto.image;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nl.ploentuin.ploentuin.model.Image;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ImageResponseDto {
+
+    private int id;
+    private int parentId;
+    private Image.ParentType parentType;
+    private String imageUrl;
+    private String caption;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
