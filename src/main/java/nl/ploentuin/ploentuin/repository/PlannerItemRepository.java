0ebe64f0e960f465109e1394a9b884c7580cc512
Zeverouis <Zeverouis@gmail.com>
2025-11-19 18:19:26 +0100

    Added UserRepository

--- src/main/java/nl/ploentuin/ploentuin/repository/PlannerItemRepository.java
+++ src/main/java/nl/ploentuin/ploentuin/repository/PlannerItemRepository.java
@@ -4,10 +4,12 @@ import nl.ploentuin.ploentuin.model.Planner;
 import nl.ploentuin.ploentuin.model.PlannerItem;
 import nl.ploentuin.ploentuin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PlannerItemRepository extends JpaRepository<PlannerItem, Integer> {
 
     List<PlannerItem> findAllByPlanner(Planner planner);
@@ -23,5 +25,4 @@ public interface PlannerItemRepository extends JpaRepository<PlannerItem, Intege
     void deleteAllByPlanner(Planner planner);
     void deleteAllByPlannerAndType(Planner planner, PlannerItem.PlannerItemType type);
     void deleteByIdAndPlannerAndPlannerUser(int id, Planner planner, User user);
-
 }
---
+++ src/main/java/nl/ploentuin/ploentuin/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package nl.ploentuin.ploentuin.repository;
+
+import nl.ploentuin.ploentuin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByUsernameIgnoreCase(String username);
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    List<User> findAllByRole(User.Role role);
+
+    List<User> findAllByEmailVerified(boolean verified);
+
+    boolean existsByUsernameIgnoreCase(String username);
+    boolean existsByEmailIgnoreCase(String email);
+}
